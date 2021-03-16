1dd9f3217d426cb94c26ee5fe1fd49e3afa0e839
nestornt <nestor1704@gmail.com>
2021-03-16 21:05:10 +0100

    Finished front-end views

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -12,12 +12,19 @@ import Menu from './core/Menu'
 const MainRouter = () => {
     return (
         <div>
+            {/* Añadimos el menu antes que el resto de rutas para que se muestre en la parte superior */}
+            <Menu/>
             {/* El componente Switch sólo renderiza el primer hijo que coincida con la ruta demandada
             para evitar el rendrizado de otras rutas que puedan coincidir */}
             <Switch>
                 <Route exact path="/" component={Home}/>
                 <Route path="/users" component={Users}/>
                 <Route path="/signup" component={Signup}/>
+                <Route path="/signin" component={Signin}/>
+                {/* La ruta privada se define antes ya que edit deriva de user para
+                 que no se confunda con esta y se buscada antes dentro del Switch */}
+                <PrivateRoute path="/user/edit/:userId" component={EditProfile}/>
+                <Route path="/user/:userId" component={Profile}/>
             </Switch>
         </div>
     )
